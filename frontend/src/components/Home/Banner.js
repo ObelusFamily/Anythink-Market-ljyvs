c8d76ba894aafe07d9c79dd0c7b55992ea8412b7
Aviad Gal <70155869+aviadg-at-kela@users.noreply.github.com>
2022-04-03 21:15:09 +0300

    Frontend - Mobile responsiveness fix (#6)
    * Fixed home page responsiveness
    * Fixed user profile page responsiveness

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 
+const logoStyle = {
+  width: "100%",
+};
+
 const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
+        <img src={logo} alt="banner" style={logoStyle} />
         <h2>A place to get the cool stuff.</h2>
       </div>
     </div>
